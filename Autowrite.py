ee6da4fa5db8c0f94ba4c2dfba9d1807985e554e
Ye Joon Kim <ericruijun@gmail.com>
2019-01-07 14:26:46 +0100

    General Structure and Formatting

---
+++ Autowrite.py
@@ -0,0 +1,60 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print("\\documentclass[11pt,a4paper]{article}")
+
+print("\\usepackage[utf8]{inputenc}")
+print("\\usepackage[T1]{fontenc}")
+print("\\usepackage{lmodern}")
+print("\\usepackage{tcolorbox}")
+
+
+print("\\usepackage[german]{babel}")
+
+
+print("\\setlength{\parindent}{0pt}")
+print("\\setlength{\parskip}{1ex plus 0.5ex minus 0.5ex}")
+
+print("\\usepackage{amsmath}")
+
+
+print("\\usepackage{graphicx}")
+
+print("\\usepackage[section]{placeins}")
+print("\\usepackage{booktabs}")
+print("\\begin{document}")
+
+
+	
+print("{")
+print("\\centering")
+print("\\large")
+print("Physiklabor für Anfänger*innen \\\\")
+print("Ferienpraktikum im Sommersemester 2018 \\\\[4mm]")
+print("\\textbf{\\LARGE ")
+print("	Versuch 75: Lichtmikroskop")
+print(")} \\\\[3mm]")
+print("(durchgeführt am 02.10.2018 bei Daniel Bartle) \\\\")
+print("Ye Joon Kim, Marouan Zouari\\\\")
+print("\\today \\\[10mm]")
+print("}")
+
+print("\\section{Einleitung}")
+print("\\section{Aufbau}")
+print("\\section{Durchführung}")
+print("\\section{Auswertung und Fehleranalyse}")
+print("\\section{Diskussion der Ergebnisse}")
+print("\\section{Literatur}")
+
+
